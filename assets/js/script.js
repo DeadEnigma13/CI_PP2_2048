9e0b61e5c955730de7df52eae9a6a7a93c970fb2
Zack Owen <zackowen15@gmail.com>
2022-08-22 14:23:21 +0000

    Add move right function

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,25 @@ document.addEventListener('DOMContentLoaded', () => {
             tiles[randomNumber].innerHTML = 2
         } else generate()
     }
+    // Swipe Right
+    function moveRight() {
+        for (let i = 0; i < 16; i++) {
+           if (i % 4 === 0) {
+                let totalOne = tiles[i].innerHTML
+                let totalTwo = tiles[i+1].innerHTML
+                let totalThree = tiles[i+2].innerHTML
+                let totalFour = tiles[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+                console.log(row)
+                let filteredRow = row.filter(num => num)
+                console.log(filteredRow)
+                let missingTiles = 4 - filteredRow.length
+                let blank = Array(missingTiles).fill(0)
+                console.log(blank)
+                let newRow = blank.concat(filteredRow)
+                console.log(newRow)
+           }
+        }
+    }
+    moveRight()
 })
