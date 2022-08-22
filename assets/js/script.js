902318345906ba306c8e0bb9f121c55f4e2ea983
Zack Owen <zackowen15@gmail.com>
2022-08-22 13:41:47 +0000

    Add start of game board with tiles

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.getElementById('score-container')
+    const resultDisplay = document.getElementById('result')
+    const width = 4
+    let tiles = []
+    // Game Tiles
+    function createBoard() {
+        for (let i = 0; i < width*width; i++) {
+            tile = document.createElement('div')
+            tile.innerHTML = 0
+            gridDisplay.appendChild(tile)
+            tiles.push(tile)
+        }
+    }
+})
